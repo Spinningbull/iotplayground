927fd9a912a8c719f75cf1c75d18fcb74c958171
Spinningbull <todd@hayfamily.com>
2016-01-28 16:56:42 -0800

    Added mqtt support to test shadow devices in AWS IOT

--- node/index.js
+++ node/index.js
@@ -1,4 +1,38 @@
 // Using mqtt to trigger and respond to aws shadow device events
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://A29TWRV0HPHLWG.iot.us-east-1.amazonaws.com');
+var fs = require('fs');
+var deviceTopic = "$aws/things/ardTest/shadow/update";
 
+var client = mqtt.connect('mqtts://A29TWRV0HPHLWG.iot.us-east-1.amazonaws.com', {
+	port: '8883',
+	cert: fs.readFileSync('/Users/toddhay/Development/iot/aws-iot-playground/cert.pem'),
+	key: fs.readFileSync('/Users/toddhay/Development/iot/aws-iot-playground/privateKey.pem')
+	}
+);
+
+//console.log (client);
+
+client.on('connect', function(){
+	console.log("Connected");
+	client.subscribe(deviceTopic+'/accepted');
+	client.subscribe(deviceTopic+'/rejected');
+	var data = {
+		state: {
+			reported: {
+				data: 'blue',
+				on: false
+			}
+		}
+	};
+	console.log(JSON.stringify(data));
+	client.publish(deviceTopic, JSON.stringify(data));
+});
+
+client.on('message', function(topic, msg) {
+	console.log(topic);
+	console.log(msg.toString());
+});
+
+client.on('error', function(err) {
+	console.log('Error: '+err);
+})
